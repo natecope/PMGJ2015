b1c495b2a955993f54763b295c2bade5497abf4e
Nathan Cope <natecope@gmail.com>
2015-03-28 13:52:15 -0400

    Beginnings of map gen
    started level manager
    test tile asset

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	// reference to camera in order to position correctly with map
+	public GameObject mainCamera;
+
+	// camera distance
+	public float cameraDistance = -5.0f;
+	public GameObject leftWallPrefab;
+	public GameObject rightWallPrefab;
+	public GameObject backgroundPrefab;
+
+	public float fallSpeed; 
+
+	// excludes left and right wall tile
+	public float mapWidth;
+	public float mapHeight;
+
+
+	GameObject[,] mapArray;
+
 	// Use this for initialization
 	void Start () {
+
+		// position cameran to center of map
+		mainCamera.transform.position = new Vector3(mapWidth / 2, mapHeight / 2, cameraDistance);
+
+		for(int y = 0; y < mapHeight; y++){
+			for(int x = 0; x < mapWidth; x++){
+				Instantiate (backgroundPrefab, new Vector2(x,y), backgroundPrefab.transform.rotation);
+			}
+		}
 	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
 	}
+
+	// spawn level tiles based on width
+	void SpawnLevel() {
+
+
+	}
+
 }
