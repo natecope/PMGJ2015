ed4f8029248c8b20922db841fa1313ad086e309c
Chrs2021 <chrs2021@gmail.com>
2015-03-28 15:39:13 -0400

    fixes

--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -6,7 +6,7 @@ public class ProjectileBehavior : MonoBehaviour {
 	public  int TTL;
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(TTLDestroy());
+//		StartCoroutine(TTLDestroy());
 	}
 	
 	// Update is called once per frame
